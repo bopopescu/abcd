6a9c7aafb35106a392090c2a4de97969c872e1d5
igareck99 <56981581+igareck99@users.noreply.github.com>
2019-10-25 01:59:34 +0300

    Initial commit

---
+++ venv/main.py
@@ -0,0 +1,7 @@
+from app import app
+import view
+
+
+from config import Configuration
+if __name__ =="__main__":
+    app.run(debug=True)
---
+++ venv/view.py
@@ -0,0 +1,6 @@
+from app import app
+from flask import render_template
+@app.route('/')#корень сайта,домен второго уровня
+def index():
+    name = 'Igor'
+    return (render_template('index.html',n = name))
