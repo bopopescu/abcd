3182d71417f0b09f71562d3541d69d859aaca8a7
igareck99 <56981581+igareck99@users.noreply.github.com>
2019-10-25 02:54:24 +0300

    building1

--- venv/view.py
+++ venv/view.py
@@ -3,4 +3,4 @@ from flask import render_template
 @app.route('/')#корень сайта,домен второго уровня
 def index():
     name = 'Igor'
-    return (render_template('index.html',n = name))
+    return (render_template('index.html',n = name));
